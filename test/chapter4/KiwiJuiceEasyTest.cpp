f00f8f4b0918edcca0b75986f85ef9eaf28fd631
ksomemo <kntsmy@hotmail.co.jp>
2013-09-23 15:30:37 +0900

    容量が満たされない場合のテストコードを実装した。

--- test/chapter4/KiwiJuiceEasyTest.cpp
+++ test/chapter4/KiwiJuiceEasyTest.cpp
@@ -41,3 +41,27 @@ TEST_F(KiwiJuiceEasyTest, toOver) {
 	EXPECT_EQ(expected, actual);
 }
 
+TEST_F(KiwiJuiceEasyTest, toUnfulfilled) {
+	vector <int> capacities, bottles, fromId, toId;
+	capacities.push_back(20);
+	capacities.push_back(20);
+
+	bottles.push_back(5);
+	bottles.push_back(8);
+
+	fromId.push_back(0);
+
+	toId.push_back(1);
+
+	vector <int> expected, actual;
+	KiwiJuiceEasy *sut;
+	sut = new KiwiJuiceEasy();
+
+	expected.push_back(0);
+	expected.push_back(13);
+	actual = sut->thePouring(capacities, bottles, fromId, toId);
+	delete sut;
+
+	EXPECT_EQ(expected, actual);
+}
+
