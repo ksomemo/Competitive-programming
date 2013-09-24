11a9f56b2bf45fb636072fcee5d5cc1b4d13f778
ksomemo <kntsmy@hotmail.co.jp>
2013-09-25 03:27:52 +0900

    それぞれの興味の中から一番多いものを返すように実装した。
    mapによる実装

--- src/chapter5/InterestingParty.cpp
+++ src/chapter5/InterestingParty.cpp
@@ -1,10 +1,40 @@
 #include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 class InterestingParty {
 public:
 	int bestInvitation(vector <string> first, vector <string> second) {
-		return 4;
+		if (first.size() != second.size()) {
+			throw "interesting count is invalid.";
+		}
+
+		map<string, int> sameInterestingCnts;
+		for (int fi = 0; fi < first.size(); fi++) {
+			if (sameInterestingCnts.find(first[fi]) != sameInterestingCnts.end()) {
+				sameInterestingCnts[first[fi]] += 1;
+			} else {
+				sameInterestingCnts.insert(pair<string, int>(first[fi], 1));
+			}
+		}
+		for (int si = 0; si < second.size(); si++) {
+			if (sameInterestingCnts.find(second[si]) != sameInterestingCnts.end()) {
+				sameInterestingCnts[second[si]] += 1;
+			} else {
+				sameInterestingCnts.insert(pair<string, int>(second[si], 1));
+			}
+		}
+
+		int maxCnt = 1;
+		map<string, int>::iterator it = sameInterestingCnts.begin();
+		while (it != sameInterestingCnts.end()) {
+			maxCnt = max(maxCnt, it->second);
+			it++;
+		}
+
+		return maxCnt;
 	}
 };
 
--- test/chapter5/InterestingPartyTest.cpp
+++ test/chapter5/InterestingPartyTest.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 #include "gtest/gtest.h"
 #include "chapter5/InterestingParty.cpp"
 
@@ -35,4 +36,20 @@ TEST_F(InterestingPartyTest, all) {
 
 	EXPECT_EQ(sut->bestInvitation(first, second), 4);
 }
+
+TEST_F(InterestingPartyTest, one) {
+	vector <string> first, second;
+
+	first.push_back("variety");
+	first.push_back("diversity");
+	first.push_back("loquacity");
+	first.push_back("courtesy");
+	second.push_back("talking");
+	second.push_back("spearking");
+	second.push_back("discussion");
+	second.push_back("meeting");
+
+	EXPECT_EQ(sut->bestInvitation(first, second), 1);
+}
+
 
