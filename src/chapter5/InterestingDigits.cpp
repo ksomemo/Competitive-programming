6f5f4b0d246c79f33733dfb7567ba199c0711d33
ksomemo <kntsmy@hotmail.co.jp>
2013-09-28 18:44:23 +0900

    n進数(3 <= n < 30)の各桁の総和を割り切れる10進数を求める問題の仮実装をした。

---
+++ src/chapter5/InterestingDigits.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+class InterestingDigits {
+public:
+	std::vector <int> digits(int base) {
+		std::vector <int> digits;
+		digits.push_back(3);
+		digits.push_back(9);
+
+		return digits;
+	}
+};
---
+++ test/chapter5/InterestingDigitsTest.cpp
@@ -0,0 +1,33 @@
+#include "chapter5/InterestingDigits.cpp"
+#include "gtest/gtest.h"
+#include <vector>
+
+class InterestingDigitsTest : public ::testing::Test {
+protected:
+	InterestingDigits *sut;
+
+	InterestingDigitsTest() {
+	}
+
+	virtual ~InterestingDigitsTest() {
+	}
+
+	virtual void SetUp() {
+		sut = new InterestingDigits();
+	}
+
+	virtual void TearDown() {
+		delete sut;
+	}
+};
+
+TEST_F(InterestingDigitsTest, base10) {
+	int base = 10;
+	std::vector <int> actual = sut->digits(base);
+	std::vector <int> expected;
+	expected.push_back(3);
+	expected.push_back(9);
+
+	EXPECT_EQ(actual, expected);
+}
+
