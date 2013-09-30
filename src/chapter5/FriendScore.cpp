7490ae59861b381b305085b19aeeee8bdf28b7ef
ksomemo <kntsmy@hotmail.co.jp>
2013-10-01 03:46:47 +0900

    全探索をして最大の友人数を数えるように変更した。

--- src/chapter5/FriendScore.cpp
+++ src/chapter5/FriendScore.cpp
@@ -7,9 +7,8 @@ public:
 	int highestScore(std::vector <std::string> friends) {
 		int highScore = 0;
 		for (int i = 0; i < friends.size(); i++) {
-			// 自分とは友人でないので、次の人から
 			int score = 0;
-			for (int j = i + 1; j < friends[i].length(); j++) {
+			for (int j = 0; j < friends[i].length(); j++) {
 				if (friends[i][j] == 'Y') score++;
 			}
 
