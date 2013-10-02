a7972a2e6060fc019ca875c2d5c51a98ff19f393
ksomemo <kntsmy@hotmail.co.jp>
2013-10-03 05:24:59 +0900

    進む方角が直行しているときの実装をした。

--- src/chapter5/CrazyBot.cpp
+++ src/chapter5/CrazyBot.cpp
@@ -3,6 +3,14 @@ public:
 	double getProbability(int step, int east, int west, int south, int north) {
 		if (step == 1) return 1.0;
 
+		// 進む方角が直行している場合
+		if (east == 100 || west == 100 || south == 100 || north == 100) return 1.0;
+
+		if (east >  0 && west == 0 && south >  0 && north == 0) return 1.0;
+		if (east == 0 && west >  0 && south >  0 && north == 0) return 1.0;
+		if (east >  0 && west == 0 && south == 0 && north >  0) return 1.0;
+		if (east == 0 && west >  0 && south == 0 && north >  0) return 1.0;
+
 		int pointPetternCnt = 4;
 		int success = 0;
 		int failure = 0;
--- test/chapter5/CrazyBotTest.cpp
+++ test/chapter5/CrazyBotTest.cpp
@@ -32,3 +32,26 @@ TEST_F(CrazyBotTest, twoStepAllCompassPoint) {
 	EXPECT_EQ(actual, expected);
 }
 
+TEST_F(CrazyBotTest, sevenStepNoCross) {
+	double actual1 = sut->getProbability(7, 50, 0,  0,  50);
+	double actual2 = sut->getProbability(7, 0,  50, 0,  50);
+	double actual3 = sut->getProbability(7, 50, 0,  50, 0);
+	double actual4 = sut->getProbability(7, 0,  50, 50, 0);
+
+	double actual5 = sut->getProbability(7, 100, 0,   0,   0);
+	double actual6 = sut->getProbability(7, 0,   100, 0,   0);
+	double actual7 = sut->getProbability(7, 0,   0,   100, 0);
+	double actual8 = sut->getProbability(7, 0,   0,   0,   100);
+
+	double expected = 1.0;
+
+	EXPECT_EQ(actual1, expected);
+	EXPECT_EQ(actual2, expected);
+	EXPECT_EQ(actual3, expected);
+	EXPECT_EQ(actual4, expected);
+	EXPECT_EQ(actual5, expected);
+	EXPECT_EQ(actual6, expected);
+	EXPECT_EQ(actual7, expected);
+	EXPECT_EQ(actual8, expected);
+}
+
