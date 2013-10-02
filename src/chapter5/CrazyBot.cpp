b671a5920e42ccd064acc2b2872e5127742a1e09
ksomemo <kntsmy@hotmail.co.jp>
2013-10-03 04:22:51 +0900

    ロボットが正しく歩ける確率を求める関数を仮実装した。
    確率はパラメータに依存するため、ランダム性なしである。

---
+++ src/chapter5/CrazyBot.cpp
@@ -0,0 +1,7 @@
+class CrazyBot {
+public:
+	double getProbability(int step, int east, int west, int south, int north) {
+		return 1.0;
+	}
+};
+
---
+++ test/chapter5/CrazyBotTest.cpp
@@ -0,0 +1,28 @@
+#include "chapter5/CrazyBot.cpp"
+#include "gtest/gtest.h"
+
+class CrazyBotTest : public ::testing::Test {
+protected:
+	CrazyBot* sut;
+
+	CrazyBotTest() {
+	}
+
+	virtual ~CrazyBotTest() {
+	}
+
+	virtual void SetUp() {
+		sut = new CrazyBot();
+	}
+
+	virtual void TearDown() {
+		delete sut;
+	}
+};
+
+TEST_F(CrazyBotTest, OneStep) {
+	double actual = sut->getProbability(1, 25, 25, 25, 25);
+	double expected = 1.0;
+	EXPECT_EQ(actual, expected);
+}
+
