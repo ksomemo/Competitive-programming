24e34eb21ef0eee7d91f38c515ecbf5c8e3fa23d
ksomemo <kntsmy@hotmail.co.jp>
2013-10-08 01:52:24 +0900

    幅優先探索による見つけ方に変更した。

--- test/chapter5/MazeMakerTest.cpp
+++ test/chapter5/MazeMakerTest.cpp
@@ -1,6 +1,7 @@
 #include "chapter5/MazeMaker.cpp"
 #include "gtest/gtest.h"
 #include <string>
+#include <vector>
 
 class MazeMakerTest : public ::testing::Test {
 protected:
@@ -22,17 +23,47 @@ protected:
 };
 
 TEST_F(MazeMakerTest, _3x3MazeMoveToUpDownRightLeftOneStep) {
-	std::string maze[] = {	"...",
-							"...",
-							"..."};
+	std::vector <std::string> maze;
+	maze.push_back("...");
+	maze.push_back("...");
+	maze.push_back("...");
 
 	int startRow = 0;
 	int startCol = 1;
-	int moveRow[] = {1, 0, -1, 0};
-	int moveCol[] = {0, 1, 0,  -1};
+	int moveRowAr[] = {1, 0, -1, 0};
+	int moveColAr[] = {0, 1, 0,  -1};
+	std::vector<int> moveRow, moveCol;
+
+	for (int i = 0; i < 4; i++) {
+		moveRow.push_back(moveRowAr[i]);
+		moveCol.push_back(moveColAr[i]);
+	}
 
 	int actual = sut->longestPath(maze, startRow, startCol, moveRow, moveCol);
 	int expected = 3;
+
+	EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MazeMakerTest, _3x3MazeMoveToAround8DirectionOneStep) {
+	std::vector <std::string> maze;
+	maze.push_back("...");
+	maze.push_back("...");
+	maze.push_back("...");
+
+	int startRow = 0;
+	int startCol = 1;
+	int moveRowAr[] = {1, 0, -1, 0,  1,  1, -1, -1};
+	int moveColAr[] = {0, 1, 0,  -1, 1, -1,  1, -1};
+	std::vector<int> moveRow, moveCol;
+
+	for (int i = 0; i < 8; i++) {
+		moveRow.push_back(moveRowAr[i]);
+		moveCol.push_back(moveColAr[i]);
+	}
+
+	int actual = sut->longestPath(maze, startRow, startCol, moveRow, moveCol);
+	int expected = 2;
 
 	EXPECT_EQ(actual, expected);
 }
