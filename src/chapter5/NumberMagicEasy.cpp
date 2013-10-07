b423073acd3ce66bcbf4765558601502ac2069d6
ksomemo <kntsmy@hotmail.co.jp>
2013-10-08 02:53:33 +0900

    質問に対する答え（Y/N)を2進数に対応づけて数をあてるように実装した。

--- src/chapter5/NumberMagicEasy.cpp
+++ src/chapter5/NumberMagicEasy.cpp
@@ -3,7 +3,21 @@
 class NumberMagicEasy {
 public:
 	int theNumber(std::string answer) {
-		return 8;
+		int minNumber = 1;
+		int number = 0;
+		int answerLen = answer.length();
+		for (int i = 0; i < answerLen; i++) {
+			if (answer[i] == 'N') {
+				int add = 1;
+				for (int j = 0; j < answerLen - i; j++) {
+					if (j == 0) continue;
+					add *= 2;
+				}
+				number += add;
+			}
+		}
+
+		return number + minNumber;
 	}
 };
 
