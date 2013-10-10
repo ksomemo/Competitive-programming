27701420548b1c414cc803b59bb014e05b14866a
ksomemo <kntsmy@hotmail.co.jp>
2013-10-11 00:59:37 +0900

    周長を最大にする辺の組み合わせを返すように実装した。
    テストコードでvectorをsortしているのは、順番が異なると失敗として扱われるため。

--- src/chapter1/TriangleMaxPerimeter.cpp
+++ src/chapter1/TriangleMaxPerimeter.cpp
@@ -4,9 +4,37 @@ class TriangleMaxPerimeter {
 public:
 	std::vector<int> sidesMaximizedPerimeter(std::vector<int> inputSides) {
 		std::vector<int> sides;
-		sides.push_back(3);
-		sides.push_back(4);
-		sides.push_back(5);
+		sides.push_back(0);
+		sides.push_back(0);
+		sides.push_back(0);
+		int sidesNum = inputSides.size();
+
+		if (sidesNum < 3) return sides;
+
+		int maxPerimeter = 0;
+		for (int i = 0; i < sidesNum; i++) {
+			for (int j = 0; j < sidesNum; j++) {
+				if (j == i) continue;
+				for (int k = 0; k < sidesNum; k++) {
+					if (k == j || k == i) continue;
+
+					int perimeter = inputSides[i] + inputSides[j] + inputSides[k];
+					int tmpIndex = inputSides[i] >= inputSides[j] ? i : j;
+					int longestSide = inputSides[tmpIndex] >= inputSides[k]
+										? inputSides[tmpIndex]
+										: inputSides[k];
+
+					if (longestSide >= (perimeter - longestSide)) continue;
+
+					if (perimeter > maxPerimeter) {
+						perimeter = maxPerimeter;
+						sides[0] = inputSides[i];
+						sides[1] = inputSides[j];
+						sides[2] = inputSides[k];
+					}
+				}
+			}
+		}
 
 		return sides;
 	}
--- test/chapter1/TriangleMaxPerimeterTest.cpp
+++ test/chapter1/TriangleMaxPerimeterTest.cpp
@@ -1,5 +1,6 @@
 #include "chapter1/TriangleMaxPerimeter.cpp"
 #include "gtest/gtest.h"
+#include <algorithm>
 
 class TriangleMaxPerimeterTest : public ::testing::Test {
 protected:
@@ -29,10 +30,28 @@ TEST_F(TriangleMaxPerimeterTest, validTriangle) {
 	sides.push_back(10);
 
 	std::vector<int> actual = sut->sidesMaximizedPerimeter(sides);
+	std::sort(actual.begin(), actual.end());
+
 	std::vector<int> expected;
 	expected.push_back(3);
 	expected.push_back(4);
 	expected.push_back(5);
+
+	EXPECT_EQ(actual, expected);
+}
+
+TEST_F(TriangleMaxPerimeterTest, inValidTriangle) {
+	std::vector<int> sides;
+	sides.push_back(4);
+	sides.push_back(5);
+	sides.push_back(10);
+	sides.push_back(20);
+
+	std::vector<int> actual = sut->sidesMaximizedPerimeter(sides);
+	std::vector<int> expected;
+	expected.push_back(0);
+	expected.push_back(0);
+	expected.push_back(0);
 
 	EXPECT_EQ(actual, expected);
 }
