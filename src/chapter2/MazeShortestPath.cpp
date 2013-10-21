5034887bd6621b4e8df7f2a43362d11cbac9fc5a
k_somemo <kntsmy@hotmail.co.jp>
2013-10-22 04:37:12 +0900

    幅優先探索を行い、通過ターン数を記録しながら最短ターン数を求めるようにした。

--- src/chapter2/MazeShortestPath.cpp
+++ src/chapter2/MazeShortestPath.cpp
@@ -1,9 +1,54 @@
 #include <string>
+#include <queue>
+#include <utility>
+#include <iostream>
 
 class MazeShortestPath {
 public:
+	typedef std::pair<int, int> pos;
+
 	int shortestTurn(std::string maze[]) {
-		return 16;
+		// 開始位置および通過ターン数の初期設定
+		int sx, sy, hMax = 10, wMax = 10;
+		int turn[hMax][wMax];
+		std::queue<pos> que;
+		for (int h = 0; h < hMax; h++) {
+			for (int w = 0; w < wMax; w++) {
+				turn[h][w] = -1;
+				if (maze[h][w] == 'S') {
+					que.push(pos(w, h));
+					turn[h][w] = 0;
+				}
+			}
+		}
+
+		while (que.size() > 0) {
+			// 移動対象
+			int x = que.front().first;
+			int y = que.front().second;
+			que.pop();
+
+			// ゴール判定
+			if (maze[y][x] == 'G') return turn[y][x];
+
+			// 移動予定追加
+			int dx[] = {0,  0, -1, 1};
+			int dy[] = {-1, 1, 0,  0};
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (0 <= nx && nx < wMax &&
+					0 <= ny && ny < hMax &&
+					turn[ny][nx] == -1 &&
+					maze[ny][nx] != '#') {
+
+					que.push(pos(nx, ny));
+					turn[ny][nx] = turn[y][x] + 1;
+				}
+			}
+		}
+
+		return 0;
 	}
 };
 
