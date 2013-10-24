fca5635a64e14abbe7cf9ece87dde756822134c7
k_somemo <kntsmy@hotmail.co.jp>
2013-10-25 01:52:03 +0900

    実装した。

--- src/chapter2/BestCowLine.cpp
+++ src/chapter2/BestCowLine.cpp
@@ -1,9 +1,37 @@
 #include <string>
+#include <iostream>
 
 class BestCowLine {
 public:
 	std::string bestCowLine(std::string str) {
-		return "ABCBCD";
+		std::string bestStr = "";
+		int len = str.length();
+		while (len > 0) {
+			bool isSmallLeft = true;
+			for (int i = 0; i < len / 2; i++) {
+				if (str[i] < str[len - i - 1]) {
+					break;
+				} else if (str[i] > str[len - i - 1]) {
+					isSmallLeft = false;
+					break;
+				}
+			}
+
+			if (len == 1) {
+				bestStr += str[0];
+				str = "";
+			} else if (isSmallLeft) {
+				bestStr += str[0];
+				str = str.substr(1, len - 1);
+			} else {
+				bestStr += str[len - 1];
+				str = str.substr(0, len - 1);
+			}
+
+			len = str.length();
+		}
+
+		return bestStr;
 	}
 };
 
