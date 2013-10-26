2f60453a9e7bf179b857a82d781782f1d567cdb8
k_somemo <kntsmy@hotmail.co.jp>
2013-10-26 19:10:00 +0900

    長い板から切断することでコストを下げるようにした。

--- src/chapter2/FenceRepair.cpp
+++ src/chapter2/FenceRepair.cpp
@@ -1,7 +1,24 @@
+#include <algorithm>
+#include <iostream>
+
 class FenceRepair {
 public:
 	int lowestCostCut(int lenghs[], int lenNum) {
-		return 34;
+		std::sort(lenghs, lenghs + lenNum, std::greater<int>());
+
+		int cost = 0;
+		int sum = 0;
+		for (int i = 0; i < lenNum; i++) {
+			sum += lenghs[i];
+		}
+
+		// 最後の板は割る必要がない
+		for (int i = 0; i < lenNum - 1; i++) {
+			cost += sum;
+			sum -= lenghs[i];
+		}
+
+		return cost;
 	}
 };
 
