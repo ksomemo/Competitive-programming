74115ca056b3dca5c90653fd364682c3204ee24a
ksomemo <kntsmy@hotmail.co.jp>
2015-06-14 01:10:46 +0900

    Slide通りのN^2で解いた

--- contest/typical/001/c_fast_fourier_transform.py
+++ contest/typical/001/c_fast_fourier_transform.py
@@ -17,17 +17,13 @@ for m, s in [input_int_l() for _ in range(N)]:
 	mains.append(m)
 	subs.append(s)
 
-def f(price):
-	type_num = 0
-	max_p = N if price > N else price
-	for i in range(1, max_p+1):
-		m_price = i
-		s_price = price - i
-		if 1 <= m_price <= max_p and 1 <= s_price <= max_p:
-			type_num += mains[m_price-1] * subs[s_price-1]
-	return type_num
+def multiply(g, h):
+    f = [0 for _ in range(len(g) + len(h)- 1)]
+    for i in range(len(g)):
+        for j in range(len(h)):
+            f[i+j] += g[i] * h[j]
+    return f
 
-for k in range(1, 2 * N + 1):
-	type_num = f(k)
-	print(type_num)
-
+answers = multiply(mains, subs)
+print(0)
+print('\n'.join(map(str, answers)))
