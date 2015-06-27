265cc15e74b5181f1c31ae10156f7ca4ad82411f
ksomemo <kntsmy@hotmail.co.jp>
2015-06-28 01:55:45 +0900

    Edit testcase using parameterized testing

--- contest/beginner/abc025/test_p_b.py
+++ contest/beginner/abc025/test_p_b.py
@@ -1,42 +1,26 @@
 import p_b
+import pytest
 
-def test_ex1():
-    move_min = 5
-    move_max = 10
-    patterns = [
+
+@pytest.mark.parametrize('move_min, move_max, patterns, expected', [
+    (5, 10, [
         ("East", 7),
         ("West", 3),
         ("West", 11),
-    ]
-
-    expected = "West 8"
-    actual = p_b.solve(move_min, move_max, patterns)
-    assert expected == actual
-
-def test_ex2():
-    move_min = 3
-    move_max = 8
-    patterns = [
+    ], "West 8"),
+    (3, 8, [
         ("West", 6),
         ("East", 3),
         ("East", 1),
-    ]
-
-    expected = "0"
-    actual = p_b.solve(move_min, move_max, patterns)
-    assert expected == actual
-
-def test_ex3():
-    move_min = 25
-    move_max = 25
-    patterns = [
+    ], "0"),
+    (25, 25, [
         ("East", 1),
         ("East", 1),
         ("West", 1),
         ("East", 100),
         ("West", 1),
-    ]
-
-    expected = "East 25"
+    ], "East 25"),
+])
+def test_parametrized(move_min, move_max, patterns, expected):
     actual = p_b.solve(move_min, move_max, patterns)
     assert expected == actual
