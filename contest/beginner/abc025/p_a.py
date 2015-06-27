9a9e13968d3f9a33be1d1131de894e6945a0eb87
ksomemo <kntsmy@hotmail.co.jp>
2015-06-28 00:29:20 +0900

    Implement solution

--- contest/beginner/abc025/p_a.py
+++ contest/beginner/abc025/p_a.py
@@ -1,2 +1,9 @@
-def solve(chars, index):
-    return "bc"
+import string
+
+def solve(fav_chars, index):
+    favs = [c for c in list(fav_chars) if c in string.ascii_lowercase]
+    candidate = []
+    for f1 in favs:
+        for f2 in favs:
+            candidate.append(f1 + f2)
+    return candidate[index-1]
--- contest/beginner/abc025/test_p_a.py
+++ contest/beginner/abc025/test_p_a.py
@@ -4,3 +4,8 @@ def test_1():
     expected = "bc"
     actual = p_a.solve("abcde", 8)
     assert expected == actual
+
+def test_ex2():
+    expected = "ue"
+    actual = p_a.solve("aeiou", 22)
+    assert expected == actual
