18da04f6066cb695b46f3d9d5a4a9cad3a82ccc9
ksomemo <kntsmy@hotmail.co.jp>
2015-07-05 01:11:48 +0900

    Add test case

--- contest/beginner/abc025/test_p_d.py
+++ contest/beginner/abc025/test_p_d.py
@@ -24,3 +24,27 @@ def test_ex2():
     expected = 40320
     actual = p_d.solve(board)
     assert expected == actual
+
+def test_ex3():
+    board = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [0, 0, 0, 0, 0],
+    ]
+    expected = 0
+    actual = p_d.solve(board)
+    assert expected == actual
+
+def test_ex4():
+    board = [
+        [1, 25, 2, 24, 3],
+        [23, 4, 22, 5, 21],
+        [6, 20, 7, 19, 8],
+        [18, 9, 17, 10, 16],
+        [11, 15, 12, 14, 13],
+    ]
+    expected = 1
+    actual = p_d.solve(board)
+    assert expected == actual
