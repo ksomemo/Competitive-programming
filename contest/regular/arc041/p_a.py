8ad9edd605c9a158a084d68b26c5391df911359d
ksomemo <kntsmy@hotmail.co.jp>
2015-07-07 02:16:44 +0900

    Add test case and Implement

--- contest/regular/arc041/p_a.py
+++ contest/regular/arc041/p_a.py
@@ -1,2 +1,6 @@
 def solve(front, back, turn):
-    return 4
+    if back >= turn:
+        return front + turn
+
+    # 裏返し切れなかった数を元に戻す
+    return front + back - (turn - back)
--- contest/regular/arc041/test_p_a.py
+++ contest/regular/arc041/test_p_a.py
@@ -8,3 +8,11 @@ def test_ex1():
     turn = 1
     actual = p_a.solve(front, back, turn)
     assert expected == actual
+
+def test_ex2():
+    expected = 3
+    front = 3
+    back = 2
+    turn = 4
+    actual = p_a.solve(front, back, turn)
+    assert expected == actual
