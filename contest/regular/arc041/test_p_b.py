f82d82718493c66b07c7e52cf905aac83b39f10f
ksomemo <kntsmy@hotmail.co.jp>
2015-07-07 06:00:59 +0900

    Add test case ex2,3

--- contest/regular/arc041/test_p_b.py
+++ contest/regular/arc041/test_p_b.py
@@ -14,3 +14,35 @@ def test_ex1():
     ]
     actual = p_b.solve(3, 3, board)
     assert expected == actual
+
+def test_ex2():
+    expected = [
+        [0, 0, 0, 0],
+        [0, 2, 3, 0],
+        [0, 0, 0, 0],
+    ]
+    board = [
+        [0, 2, 3, 0],
+        [2, 3, 2, 3],
+        [0, 2, 3, 0],
+    ]
+    actual = p_b.solve(3, 4, board)
+    assert expected == actual
+
+def test_ex3():
+    expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 2, 0, 3, 0],
+        [0, 0, 3, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    board = [
+        [0, 0, 1, 0, 0],
+        [0, 3, 0, 4, 0],
+        [2, 0, 9, 0, 3],
+        [0, 5, 0, 6, 0],
+        [0, 0, 3, 0, 0],
+    ]
+    actual = p_b.solve(5, 5, board)
+    assert expected == actual
