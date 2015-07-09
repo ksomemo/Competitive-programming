d17e80619804b309a226ae16a5a9565a05ab5a81
ksomemo <kntsmy@hotmail.co.jp>
2015-07-09 10:41:36 +0900

    Remove useless function

--- contest/regular/arc041/p_c.py
+++ contest/regular/arc041/p_c.py
@@ -89,13 +89,6 @@ def input_str_l(sep=None):
 def input_int_l(sep=None):
     return list(map(int, input_str_l(sep)))
 
-def input_int_l_nosep():
-    return list(map(int, list(input_str())))
-
-def print_nested(nested, sep=''):
-    for l in nested:
-        print(sep.join(map(str, l)))
-
 if __name__ == '__main__':
     N, L = input_int_l()
     rabbits = []
