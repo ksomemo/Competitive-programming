5ef4ce7a07b79e204492b5737042d81dc7656bf2
ksomemo <kntsmy@hotmail.co.jp>
2017-07-01 22:05:56 +0900

    Edit ABC 066 C (not using sort)

--- abc/066/C.py
+++ abc/066/C.py
@@ -1,9 +1,19 @@
 def main():
     N = int(input().strip())
-    b = []
-    for a in map(int, input().strip().split()):
-        b.append(a)
-        b = b[::-1]
+    b = [None for _ in range(N)]
+
+    index = N // 2
+    # 追加場所
+    side = -1
+    if N % 2 == 0:
+        side = 1
+
+    a = map(int, input().strip().split())
+    for i, a in enumerate(a):
+        index = index + (side * i)
+        b[index] = a
+        # 追加場所の左右反転
+        side *= -1
     print(" ".join(map(str, b)))
 
 if __name__ == '__main__':
