e4f4a76d6ffbbf47695402e13635aceaec8fa798
ksomemo <kntsmy@hotmail.co.jp>
2017-07-11 22:21:22 +0900

    Im forgot

--- contest/regular/arc041/test_p_d.py
+++ contest/regular/arc041/test_p_d.py
@@ -5,11 +5,26 @@ def test_ex1():
     """始点から終点まで一本道なのでYes"""
     expected = 'Yes'
     edges = [
-       [1, 2, 'r'],
+        [1, 2, 'r'],
         [2, 3, 'b'],
         [3, 4, 'r'],
         [4, 5, 'b'],
         [5, 6, 'r'],
     ]
     actual = p_d.solve(6, 5, edges)
+    assert expected == actual
+
+def test_ex3():
+    """循環している
+    1--2
+    |  |
+    3--|
+    """
+    expected = 'No'
+    edges = [
+        [1, 2, 'b'],
+        [1, 3, 'b'],
+        [2, 3, 'b'],
+    ]
+    actual = p_d.solve(3, 3, edges)
     assert expected == actual
