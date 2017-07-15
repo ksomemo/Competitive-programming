57555064304bf0861809ca883c0021dc6f9c21d5
ksomemo <kntsmy@hotmail.co.jp>
2017-07-16 08:43:42 +0900

    Add ABC 067 C(解説参考)

--- atcoder/abc/067/C.py
+++ atcoder/abc/067/C.py
@@ -1,11 +1,36 @@
 def main():
+    """
+    1.
+        |前半合計 - 後半合計|
+    ↓
+    2.
+        全体合計 = 前半合計 + 後半合計
+        |全体合計 - 前半合計 * 2|
+        |後半合計 - 前半合計|
+        => 1.
+
+    [計算量]
+    1.
+    N-loop:
+        sum - sum => O(N)
+
+    2.
+    1-loop: sum => O(N)
+    N-loop:
+        add
+        sum(計算済み) - cumsum * 2
+        => O(1)
+    """
     N = int(input().strip())
     a = list(map(int, input().strip().split()))
+    s = sum(a)
     min_v = float("inf")
-    for i in range(1, N):
-        x = sum(a[:i])
-        y = sum(a[i:])
-        min_v = min(min_v, abs(x - y))
+    cumsum = 0
+    for i in range(N):
+        cumsum += a[i]
+        if i >= 1:
+            min_v = min(min_v, abs(s - 2 * cumsum))
+
     print(min_v)
 
 if __name__ == '__main__':
