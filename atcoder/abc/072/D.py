2f7cc138c0699d07b80bc3dd58f30e8d0b415524
ksomemo <kntsmy@hotmail.co.jp>
2017-09-10 18:53:59 +0900

    Edit atcoder ABC 072 D(解説のpython実装)

--- atcoder/abc/072/D.py
+++ atcoder/abc/072/D.py
@@ -8,17 +8,26 @@ def main():
     p_i != i にする
     最悪、sort(reverse) + αすれば終わりそう
     -> N^2(10^5^2 -> 10^10 NG)
+
+    xx: 12(xx) -> 21(oo)
+    xo: 132(xoo) -> 312(ooo)
+    ox:
+      oは飛ばしてxから始める
     """
     N = int(input())
-    p = list(map(int, input().split()))
+    p = map(int, input().split())
     n_swap = 0
 
+    p_same = [i == _p for i, _p in enumerate(p, 1)]
+    # enumerateでは複製になるのでrangeで回しながら更新
     for i in range(N - 1):
-        for j in range(i, N - 1):
-            if p[j] == j + 1 or p[j + 1] == j + 2:
-                p[j + 1], p[j] = p[j], p[j + 1]
-                n_swap += 1
+        same = p_same[i]
+        if same:
+            n_swap += 1
+            p_same[i] = p_same[i + 1] = False
 
+    if p_same[-1]:
+        n_swap += 1
     print(n_swap)
 
 if __name__ == '__main__':
