ced20bdef19a5583d728b620fca9099a759a1ed4
ksomemo <kntsmy@hotmail.co.jp>
2017-09-17 19:28:42 +0900

    Edit atcoder ABC 074 D (Py3.4.3 TLE/PyPy3 AC)
    reference: https://beta.atcoder.jp/contests/abc074/submissions/1603928

--- atcoder/abc/074/D.py
+++ atcoder/abc/074/D.py
@@ -9,25 +9,29 @@ def main():
     N = int(input())
     a = [None] * N
     total = 0
-    no_need = 0
     for i in range(N):
         a[i] = list(map(int, input().split()))
         total += sum(a[i])
 
     for k in range(N):
-        # from
         for i in range(N):
-            # to
             for j in range(N):
                 # compare from-to / from-経由地点-to
-                if i == k or j == k:
-                    continue
                 d = a[i][k] + a[k][j]
                 if a[i][j] > d:
                     return -1
-                elif a[i][j] == d:
-                    # 経由して同じであれば必要ない
-                    no_need += d
+
+    # 不要判定
+    v = [[False] * N for _ in range(N)]
+    no_need = 0
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                if i == k or j == k:
+                    continue
+                if not v[i][j] and (a[i][j] == a[i][k] + a[k][j]):
+                    no_need += a[i][j]
+                    v[i][j] = True
 
     # 対称なので半分にする
     return (total - no_need) // 2
