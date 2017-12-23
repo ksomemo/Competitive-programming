d536840e8ef612fb96c0ba5ff4e8c20008cd1d8c
ksomemo <kntsmy@hotmail.co.jp>
2017-12-23 20:48:58 +0900

    Edit AtCoder ABC D(AC) 解説

--- atcoder/abc/082/D.py
+++ atcoder/abc/082/D.py
@@ -9,7 +9,45 @@ def main():
     s = input()
     x, y = map(int, input().split())
 
+    f(s, x, y)
+    return
     solve(s, x, y)
+
+
+def f(s, x, y):
+    # Fの数をまとめる
+    f_counts = [
+        len(fs)
+        for fs in s.split("T")
+    ]
+    # 曲がる方向が90°より、x,yに分解
+    x_counts = []
+    y_counts = []
+    for i, count in enumerate(f_counts):
+        if i % 2 == 0:
+            x_counts.append(count)
+        else:
+            y_counts.append(count)
+
+    x_dp = [{} for _ in range(len(x_counts))]
+    y_dp = [{} for _ in range(len(y_counts) + 1)]
+    pos_x = x_counts.pop(0)
+    x_dp[0][pos_x] = True
+    y_dp[0][0] = True
+
+    for i, count in enumerate(x_counts):
+        for pos in x_dp[i].keys():
+            x_dp[i + 1][pos + count] = True
+            x_dp[i + 1][pos - count] = True
+    for i, count in enumerate(y_counts):
+        for pos in y_dp[i].keys():
+            y_dp[i + 1][pos + count] = True
+            y_dp[i + 1][pos - count] = True
+
+    if x_dp[-1].get(x) and y_dp[-1].get(y):
+        print("Yes")
+    else:
+        print("No")
 
 
 def solve(s, x, y):
