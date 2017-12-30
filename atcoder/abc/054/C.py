f5f4c40ba14441521511481aac5e1173eb0fe423
ksomemo <kntsmy@hotmail.co.jp>
2017-12-30 23:06:40 +0900

    Edit AtCoder ABC 054 C(AC, 解説:dfs)

--- atcoder/abc/054/C.py
+++ atcoder/abc/054/C.py
@@ -5,5 +5,40 @@ def main():
     """
     N, M = map(int, input().split())
 
+    # 無向グラフ
+    g = [
+        [False] * N
+        for _ in range(N)
+    ]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        g[a - 1][b - 1] = True
+        g[b - 1][a - 1] = True
+
+    # 訪問状態
+    visited = [False for _ in range(N)]
+    visited[0] = True
+
+    def dfs(v, N, visited):
+        if all(visited):
+            return 1
+
+        ret = 0
+        for i in range(N):
+            # 経路存在と訪問済み確認
+            if not g[v][i] or visited[i]:
+                continue
+
+            # 再帰後に元に戻す
+            visited[i] = True
+            ret += dfs(i, N, visited)
+            visited[i] = False
+
+        return ret
+
+    ans = dfs(0, N, visited)
+
+    print(ans)
+
 if __name__ == '__main__':
     main()
