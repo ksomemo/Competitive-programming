c6c72481986f37304aad5c1112966b9ea9c8cb49
ksomemo <kntsmy@hotmail.co.jp>
2018-02-09 15:37:20 +0900

    Edit AtCoder ABC 087 D(AC:解説)

--- atcoder/abc/087/D.py
+++ atcoder/abc/087/D.py
@@ -1,14 +1,4 @@
 def main():
-    N, M = map(int, input().split())
-    lrd = [
-        tuple(map(int, input().split()))
-        for _ in range(M)
-    ]
-
-    TLE(N, M, lrd)
-
-
-def TLE(N, M, lrd):
     """
     N,M:10^6より N^2->NG
     矛盾する情報を見つける
@@ -27,7 +17,51 @@ def TLE(N, M, lrd):
     Ri < Li のとき(人8の右に人7がいる)場合、
     xLi - xRi = -Di
     """
-    pass
+    N, M = map(int, input().split())
+    lrd = [
+        tuple(map(int, input().split()))
+        for _ in range(M)
+    ]
+
+    editorial(N, M, lrd)
+
+
+def editorial(N, M, lrd):
+    import sys
+    sys.setrecursionlimit(10 ** 7)
+
+    xs = [None] * N
+    graph = [[] for _ in range(N)]
+    for l, r, d in lrd:
+        graph[l - 1].append((r - 1, d))
+        graph[r - 1].append((l - 1, -d))
+
+    def dfs(i):
+        # 辺情報がなければ独立
+        for j, d in graph[i]:
+            if xs[j] is None:
+                xs[j] = xs[i] + d
+                f = dfs(j)
+                if not f:
+                    return False
+            elif xs[j] - xs[i] != d:
+                # 距離が誤情報
+                return False
+
+        return True
+
+    for i, x in enumerate(xs):
+        if x is None:
+            # root以外は、dfsで木を探索するときに座標確定
+            # １回で確定しないときは、独立した木が存在する
+            # 他の木とは別で考えて良いので、初期座標は0固定で問題ない
+            xs[i] = 0
+            f = dfs(i)
+            if not f:
+                print("No")
+                return
+
+    print("Yes")
 
 if __name__ == '__main__':
     main()
