ddf36dd628fc220942c8f9172609db0e8b7cdeca
ksomemo <kntsmy@hotmail.co.jp>
2018-02-20 22:20:53 +0900

    Edit AtCoder ABC 076 C(AC) 本質的に合ってたけど提出ミスWA

--- atcoder/abc/076/C.py
+++ atcoder/abc/076/C.py
@@ -2,9 +2,22 @@ def main():
     S_ = input()
     T = input()
 
-    S = ""
-    # ?: a-z or UNRESTORABLE
-    print(S)
+    f(S_, T)
+
+
+def f(s, t):
+    l_t = len(t)
+    for i in reversed(range(len(s)-l_t+1)):
+        b = s[i:i+l_t]
+        c = sum(c1 == c2 or c2 == "?" for c1, c2 in zip(t, b))
+
+        if c == len(t):
+            ans = s[:i].replace("?", "a") + t + s[i+l_t:].replace("?", "a")
+            print(ans)
+            return
+
+    print("UNRESTORABLE")
+
 
 if __name__ == '__main__':
     main()
