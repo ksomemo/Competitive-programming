910317fe0ba73c684e6a81e4d9b967fc9e46da95
ksomemo <kntsmy@hotmail.co.jp>
2018-03-04 16:04:28 +0900

    Edit AtCoder ABC 075 D(TLE:解説, AC:pypy3)

--- atcoder/abc/075/D.py
+++ atcoder/abc/075/D.py
@@ -1,5 +1,3 @@
-
-
 def main():
     N, K = map(int, input().split())
     x, y = zip(*(
@@ -7,94 +5,31 @@ def main():
         for _ in range(N)
     ))
 
-    f(N, K, x, y)
+    TLE(N, K, x, y)
 
 
-def f(N, K, x, y):
+def TLE(N, K, x, y):
     """
-    制約
-    * 2≦K≦N≦50
-    * −10^9≦xi,yi≦10^9 (1≦i≦N)
-    * xi≠xj (1≦i<j≦N)
-    * yi≠yj (1≦i<j≦N)
-    * 入力値はすべて整数である。(21:50 追記)
-
-    長方形の内部にN点のうち
-    K個以上の点を含みつつ、
-    それぞれの辺がX軸かY軸に平行な長方形を考えます。
-    →最小の面積
-
-    制約より、
-    xyの範囲を全探索はTLE
-    点は重ならないかつ、50以下
-    →点の座標から求めれば間に合いそう
-
-    例題1より、
-    全ての点を含む場合、
-    abs(x_max-x_min) * abs(y_max-y_min)
-    => 追記:
-        これを分かったのに図示しなかったため、
-        点が角ではなく辺上にのることに気づかず…
-        点同士の組合せになってしまった(座標同士の組合せと気づけたはず)
-
-    例題2より
-    １つの場合1、ただし1*1の長方形より最大4点含む
-    →この例の場合、4点から2点以上で面積1
-
-    (x_2nd-x_min)*(y_max-y_min)
-    →x_maxを除いた点が含まれる
-    →50*50では済まない
-
-    y
-    ↑
-    |*...
-    |.*..
-    |...*
-    |*...
-    ------→x
-    (0,0)
-    (0,4)
-    (1,3)
-    (3,1)
-
-    各点を結んだときの面積の個数：O(N*N)
-    面積求めたあとの点が含まれる個数の計算量: O(N)
-    => O(N^3) => 50^3 => 1.25 * 10^5 => OK
-      点を必ず含む場所からの面積
-      => そこからでなくてよい, むしろ広くなる
-
-    => 
+    TLEのため、考察はpypy3のD directoryに移行
+    codeの見た目のためにgeneratorを使ったが遅いので愚直にloopにしたがTLE
+    解説の累積和をあとで試す
     """
-    ans = abs(max(x) - min(x)) * abs(max(y) - min(y))
-    if K == N:
-        print(ans)
-        return
-
-    def r(x, y):
-        for _x in x:
-            for _y in y:
-                yield _x, _y
-
-    # for x1, y1 in zip(x, y):
-    #     for x2, y2 in zip(x, y):
-    for x1, y1 in r(x, y):
-        for x2, y2 in r(x, y):
-
-            s = abs(x1-x2) * abs(y1-y2)
-            if s == 0:
-                continue
-
-            min_x = min(x1, x2)
-            max_x = max(x1, x2)
-            min_y = min(y1, y2)
-            max_y = max(y1, y2)
-
-            contains_count = 0
-            for x3, y3 in zip(x, y):
-                if min_x <= x3 <= max_x and min_y <= y3 <= max_y:
-                    contains_count += 1
-            if contains_count >= K:
-                ans = min(ans, s)
+    sorted_x = sorted(x)
+    sorted_y = sorted(y)
+    ans = (sorted_x[-1] - sorted_x[0]) * (sorted_y[-1] - sorted_y[0])
+    for xi in range(N):
+        for xj in range(xi + 1, N):
+            for yi in range(N):
+                for yj in range(yi + 1, N):
+                    min_x, max_x = sorted_x[xi], sorted_x[xj]
+                    min_y, max_y = sorted_y[yi], sorted_y[yj]
+                    contains_count = 0
+                    for x3, y3 in zip(x, y):
+                        if min_x <= x3 <= max_x and min_y <= y3 <= max_y:
+                            contains_count += 1
+                    if contains_count >= K:
+                        s = (max_x - min_x) * (max_y - min_y)
+                        ans = min(ans, s)
 
     print(ans)
 
---
+++ atcoder/abc/075/D/D.py
@@ -0,0 +1,106 @@
+def main():
+    N, K = map(int, input().split())
+    x, y = zip(*(
+        map(int, input().split())
+        for _ in range(N)
+    ))
+
+    f(N, K, x, y)
+
+
+def f(N, K, x, y):
+    """
+    制約
+    * 2≦K≦N≦50
+    * −10^9≦xi,yi≦10^9 (1≦i≦N)
+    * xi≠xj (1≦i<j≦N)
+    * yi≠yj (1≦i<j≦N)
+    * 入力値はすべて整数である。(21:50 追記)
+
+    長方形の内部にN点のうち
+    K個以上の点を含みつつ、
+    それぞれの辺がX軸かY軸に平行な長方形を考えます。
+    →最小の面積
+
+    制約より、
+    xyの範囲を全探索はTLE
+    点は重ならないかつ、50以下
+    →点の座標から求めれば間に合いそう
+
+    例題1より、
+    全ての点を含む場合、
+    abs(x_max-x_min) * abs(y_max-y_min)
+    => 追記:
+        これを分かったのに図示しなかったため、
+        点が角ではなく辺上にのることに気づかず…
+        点同士の組合せになってしまった(座標同士の組合せと気づけたはず)
+
+    例題2より
+    １つの場合1、ただし1*1の長方形より最大4点含む
+    →この例の場合、4点から2点以上で面積1
+
+    (x_2nd-x_min)*(y_max-y_min)
+    →x_maxを除いた点が含まれる
+    →50*50では済まない
+
+    y
+    ↑
+    |*...
+    |.*..
+    |...*
+    |*...
+    ------→x
+    (0,0)
+    (0,4)
+    (1,3)
+    (3,1)
+
+    各点を結んだときの面積の個数：O(N*N)
+    面積求めたあとの点が含まれる個数の計算量: O(N)
+    => O(N^3) => 50^3 => 1.25 * 10^5 => OK
+      点を必ず含む場所からの面積
+      => そこからでなくてよい, むしろ広くなる
+
+    => 
+    """
+    ans = abs(max(x) - min(x)) * abs(max(y) - min(y))
+    if K == N:
+        print(ans)
+        return
+
+    def r(x, y, x_start=0, y_start=0):
+        for xi in range(x_start, N):
+            for yi in range(y_start, N):
+                yield x[xi], y[yi], xi, yi
+
+    # for x1, y1 in zip(x, y):
+    #     for x2, y2 in zip(x, y):
+    # for x1, y1 in r(x, y):
+    #     for x2, y2 in r(x, y):
+    sorted_x = sorted(x)
+    sorted_y = sorted(y)
+    for min_x, min_y, xi, yi in r(sorted_x, sorted_y):
+        for max_x, max_y, _, _ in r(sorted_x, sorted_y, x_start=xi+1, y_start=yi+1):
+
+            # s = abs(x1-x2) * abs(y1-y2)
+            # if s == 0:
+            #     continue
+
+            # min_x = min(x1, x2)
+            # max_x = max(x1, x2)
+            # min_y = min(y1, y2)
+            # max_y = max(y1, y2)
+
+            contains_count = 0
+            for x3, y3 in zip(x, y):
+                if min_x <= x3 <= max_x and min_y <= y3 <= max_y:
+                    contains_count += 1
+            if contains_count >= K:
+                s = (max_x - min_x) * (max_y - min_y)
+                ans = min(ans, s)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
