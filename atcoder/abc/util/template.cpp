bd89a4195c301b97c26e28b64f4dbef7ff07660e
ksomemo <kntsmy@hotmail.co.jp>
2018-04-30 14:41:25 +0900

    Add C++ template and proto type function

--- atcoder/abc/util/template.cpp
+++ atcoder/abc/util/template.cpp
@@ -5,7 +5,50 @@
 using namespace std;
 using ll = long long;
 
+template <typename T>
+T proto_add(T a, T b);
+
+template <>
+int proto_add<int>(int a, int b) {
+  // error: specialization of 'T proto_add(T, T) [with T = int]' after
+  // instantiation
+  // prototype宣言していても、引数intである呼び出しコードより先に書くこと
+  int r = a + b;
+  cout << r << endl;
+  return r;
+}
+
+template <typename T, class C>
+class MyClass {
+ public:
+  T v;
+  C c;
+
+ public:
+  template <typename U>
+  U id(U x) {
+    // error: one liner
+    return x;
+  }
+};
+
 int main() {
   // template
+  cout << "start" << endl;
+
+  // error: invalid operands of types 'const char*' and 'const char*' to binary
+  // 'operator+'
+  // assert(proto_add("a", "b") == "ab");
+  assert(proto_add<string>("a", "b") == "ab");
+  assert(proto_add(1, 2) == 3);
+  assert(proto_add(1.0, 3.0) == 4.0);
+
+  cout << "end" << endl;
   return 0;
+}
+
+template <typename T>
+T proto_add(T a, T b) {
+  // error: one liner
+  return a + b;
 }
