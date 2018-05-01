f2ea3d2c2c86e7dba6d31ae5890abf5ff6105a0d
ksomemo <kntsmy@hotmail.co.jp>
2018-05-02 01:24:11 +0900

    Edit AtCoder ABC 093 C(editorial), D(答えはわかったが解説一部分からないのでACもしてない)

--- atcoder/abc/093/C.py
+++ atcoder/abc/093/C.py
@@ -1,10 +1,36 @@
 def main():
-    A,B,C = map(int, input().split())
+    A, B, C = map(int, input().split())
 
-    f(A,B,C)
+    #f(A, B, C)
+    editorial(A, B, C)
 
 
-def f(A,B,C):
+def editorial(A, B, C):
+    """
+    fより、最終値をxとするとx={max, max + 1}
+    操作回数: [3x - (A+B+C)] / 2
+    1回の操作で2増えるので
+        差分/2が回数となる
+        3x mod 2 == abc mod 2
+
+    3max mod 2 == abc mod 2
+        true: 上記の通りのあまりの一致
+        false: x=max+1より、全体では3max+3より奇数増えるので一致しない
+    """
+    abc = [A, B, C]
+    m = max(abc)
+    m3 = m * 3
+    s = sum(abc)
+
+    if m3 % 2 == s % 2:
+        ans = (m3 - s) // 2
+    else:
+        ans = (m3 + 3 - s) // 2
+
+    print(ans)
+
+
+def f(A, B, C):
     """
     A: 545, 565, 666 ans=2
     B: 445,      555 ans=1
@@ -18,7 +44,7 @@ def f(A,B,C):
         ans += C - B
         B = C
         A += ans
-    
+
     diff = B - A
     ans += diff // 2
     if diff % 2 != 0:
--- atcoder/abc/093/D.py
+++ atcoder/abc/093/D.py
@@ -40,9 +40,106 @@ def f(Q, AB):
         print(ans)
 
 
+def editorial_pdf(A, B):
+    """
+    B=A+1
+        score: A(A+1)
+        A-1まで:ok
+        A+1以上: socre=A(A+1) よりA-1以内
+    
+    C(C+1) または C^2 が積の最大値となることを利用する方法
+    C^2<AB を満たす C を取ると、
+        1: C^2<AB<C(C+1)
+            A=3,B=13,√36=6<√39より、6^2<AB=39<6*7=42
+        2: C^2   <C(C+1)<AB
+            A=3,B=12,√36=6よりC=5,5^2=25<5*6=30<AB
+    
+        A  =B,C=A,A^2=A^2    C--する理由
+        A+1=B,C=A,A^2<A(A+1) 
+        のときはC=Aを含むため例外としている
+    => これがベース
+        だとしても、A-1までは確定
+        A+1以降:
+            要素数が奇数
+            A+1,A+2,...,C-1,C,C+1,...,X
+            X-A,.....................,1
+        要素数が偶数
+            A+1以降:A+1,A+2,...,C-1,C
+
+        2C-1,2C-2が出て来る理由
+            2(A-1)と同じ
+                2(A-1):A=BのときA-1人Aより小さい人がいて、2回目についても同様に考えるため
+            C(C+1)<=AB
+                C位以内とC位以内
+                ただしAとBが含まれるのでそれぞれ-1で2(C-1)
+            C^2
+                C位以内とC-1位以内
+
+        1,    2,    ...,A-1
+        A+B-1,A+B-2,...,B+1
+            A+B-1?,not 2C-1?
+            (C,C)が途中であらわれる
+            (A+1,2C-A-1) 分かる:(1,2C-1)のあとA
+            (A-1,B+1)    分からない
+            (A-1)(B+1)
+            = AB + (A-B) - 1 <=AB-1 (A<B)より
+                まだ分からない 
+            (A-1)B
+            = AB -B
+    """
+    if A > B:
+        A, B = B, A
+        score = A * B
+        if A == B or A + 1 == B:
+            return (A - 1) * 2
+
+    _C = score ** 0.5
+    C = int(_C)
+    if C == _C:
+        C -= 1
+
+    if C * (C + 1) >= score:
+        return 2 * C - 2
+    else:
+        return 2 * C - 1
+
+
+def editorial_movie(A, B):
+    """
+    https://www.youtube.com/watch?v=HDRfgn_UXLE
+    http://banboooo.hatenablog.com/entry/2018/04/08/191942
+    http://yamakasa3.hatenablog.com/entry/2018/04/22/000659
+    http://kmjp.hatenablog.jp/entry/2018/04/07/0900
+    http://ferin-tech.hatenablog.com/entry/2018/04/15/150222
+    https://twitter.com/chiguri/status/982616224931577856
+
+    score: AB
+    1*x1 < AB => x1=AB-1 => x1=(AB-1)/1
+    2*x2 < AB => x2=AB-1 => x2=(AB-1)/2
+    ...
+    (A-1)*x=AB           =>  x=(AB-1)/(A-1)
+        ここまでA-1人
+    A+1からAB-1まで
+        A=Bのとき,2(A-1)
+        平方完成での説明
+
+    解説動画のクロスさせてABより小さくすることについて
+    上から3人の例は、あくまで例
+        A<=Bのとき A-B<=0 より
+        (A-1)(B+1) = AB +  (A-B) - 1   <=AB-1
+        (A-2)(B+2) = AB + 2(A-B) - 4   <=AB-4
+        (A-x)(B+x) = AB + x(A-B) - x^2 <=AB-1 (x>=1)
+
+        A+1
+    """
+    ans = 0
+
+    return ans
+
+
 def solve(A, B):
     score = A * B
-    a, b = sorted([A ,B], reverse=True)
+    a, b = sorted([A, B], reverse=True)
     # sortは意味がない
     a, b = A, B
 
