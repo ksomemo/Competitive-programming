5ea7cfda327305a6a41817266d2c84b93bfe5c4a
ksomemo <kntsmy@hotmail.co.jp>
2018-09-08 15:58:51 +0900

    Edit AtCoder ABC 108 A,B,C(AC:editorial)

--- atcoder/abc/108/A.py
+++ atcoder/abc/108/A.py
@@ -1,6 +1,28 @@
 def main():
     K = int(input())
 
+    ans = editorial(K)
+    assert ans == AC(K) == loop(K)
+    print(ans)
+
+
+def editorial(K):
+    """
+    K % 2 == 0: 奇数も偶数も同じ個数、K+1//2しても結果は変わらない 
+    K % 2 == 1: 奇数のほうが多い、Kは奇数なのでK//2では繰り下がりK+1//2は繰り上がる
+    """
+    return (K // 2) * ((K + 1) // 2)
+
+
+def loop(K):
+    """
+    (K // 2) * (K - K // 2)
+    """
+    even = sum(1 for i in range(1, K+1) if i % 2 == 0)
+    return even * (K - even)
+
+
+def AC(K):
     d = {}
     for i in range(1, K+1):
         for j in range(i+1, K+1):
@@ -11,7 +33,7 @@ def main():
                 d[p] = 1
 
     ans = len(d)
-    print(ans)
+    return ans
 
 
 if __name__ == '__main__':
--- atcoder/abc/108/B.py
+++ atcoder/abc/108/B.py
@@ -1,8 +1,69 @@
+import pytest
+
+
 def main():
     x1, y1, x2, y2 = map(int, input().split())
 
-    x3, y3, x4, y4 = f(x1, y1, x2, y2)
-    print(x3, y3, x4, y4)
+    ans = editorial(x1, y1, x2, y2)
+    print(*ans)
+
+
+@pytest.mark.randomize(x1=int, y1=int, x2=int, y2=int,
+                       min_num=-(10 ** 2), max_num=10 ** 2)
+def test_ans(x1, y1, x2, y2):
+    assert editorial(x1, y1, x2, y2) == twi(x1, y1, x2, y2)
+
+
+def twi(x1, y1, x2, y2):
+    """
+                [2]
+              (-1,+1)
+                 |   
+    -[3]:(-2, 0)-+-( 0, 0):[1]-
+                 | 
+              (-1,-1)
+                [4]
+ 
+    dx = -1
+    dy = +1
+                [2]
+              (+2,+3)
+                 |   
+    -[3]:(+1,+2)-+-(+3,+2):[1]-
+                 | 
+              (+2,+1)
+                [4]
+ 
+    https://twitter.com/akensho/status/1035924165205716992
+    https://twitter.com/hogeover30/status/1035903937830764545
+    """
+    import math
+    cos90 = int(math.cos(math.pi / 2))
+    sin90 = int(math.sin(math.pi / 2))
+
+    x = x2 - x1
+    y = y2 - y1
+    x4 = x * cos90 - y * sin90 + x1
+    y4 = x * sin90 + y * cos90 + y1
+
+    x = x1 - x4
+    y = y1 - y4
+    x3 = x * cos90 - y * sin90 + x4
+    y3 = x * sin90 + y * cos90 + y4
+
+    return x3, y3, x4, y4
+
+
+def editorial(x1, y1, x2, y2):
+    x = x2 - x1
+    y = y2 - y1
+
+    x3 = x2 - y
+    y3 = y2 + x
+    x4 = x1 - y
+    y4 = y1 + x
+
+    return x3, y3, x4, y4
 
 
 def f(x1, y1, x2, y2):
