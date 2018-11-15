765766d73fd4f72a2e597a9b866e8c7776d66eac
ksomemo <kntsmy@hotmail.co.jp>
2018-11-16 07:21:50 +0900

    Edit AtCoder ABC 113 D(AC:editorial)

--- atcoder/abc/113/D.py
+++ atcoder/abc/113/D.py
@@ -10,7 +10,8 @@ def main():
     """
     H, W, K = map(int, input().split())
 
-    ans = f(H, W, K)
+    # ans = visualize(H, W, K)
+    ans = editorial(H, W, K)
     print(ans)
 
 
@@ -44,6 +45,89 @@ def f(H, W, K):
     ans = 0
 
     return ans % mod
+
+
+def visualize(H, W, K):
+    valid_k = []
+    for k in range(1 << (W - 1)):
+        if not k & (k << 1):
+            valid_k.append(k)
+
+    for i in range(H):
+        print("row", i+1)
+        for j in range(W):
+            for k in valid_k:
+                h_bar = "{:08b}".format(k)[-(W-1):]
+                h_bar = h_bar.replace("1", "_").replace("0", " ")
+
+                header = " " + " ".join(str(i) for i in range(W)) + " "
+                header = header.replace(" {} ".format(j), "[{}]".format(j))
+
+                print(header)
+                print(" |" + "|".join(list(h_bar)) + "| ")
+                print(" " + " ".join("|" for _ in range(W)) + " ")
+                print()
+
+    return 1
+
+
+def is_connected(k, W):
+    # 2つの横線がつながっていないか
+    ok = True
+    for l in range(W - 2):
+        if k >> l & 1 and k >> (l + 1) & 1:
+            ok = False
+            break
+
+    ok2 = "11" not in bin(k)
+    ok3 = not (k & (k << 1))
+    assert ok == ok2 == ok3
+
+    return ok
+
+
+def editorial(H, W, K):
+    mod = 10 ** 9 + 7
+    dp = [[0] * W for _ in range(H+1)]
+    dp[0][0] = 1
+    # dp[H+1][W]
+    # dp[i][j]: 上からicmまでの時点で、1からjに行ける数
+    # dp[i + 1][a] += dp[i][j]: 下に1つ進んだとき、aに行ける数
+    for i in range(H):
+        for j in range(W):
+            k_max = 1 << (W - 1)
+            for k in range(k_max):
+                # 2つの横線がつながっていないか
+                ok = is_connected(k, W)
+                if not ok:
+                    continue
+
+                #  pos:            L   S   R
+                #  v_bar:   [j-1]_____[j]_____[j+1]
+                #  h_bar:         j-1     j
+                a = 0  # straight
+                if j >= 1 and k >> (j - 1) & 1:  # left
+                    a = - 1
+                elif j < W - 1 and k >> j & 1:  # right
+                    a = 1
+
+                dp[i + 1][j + a] += dp[i][j]
+                dp[i + 1][j + a] %= mod
+
+    return dp[H][K - 1]
+
+
+def a(H, W, K):
+    """
+    https://twitter.com/Py2K4/status/1059078331407028226
+    """
+
+
+def editorial_fib(H, W, K):
+    """
+    https://twitter.com/259_Momone/status/1059085685208956928
+    導出: 棒が2本連続しない
+    """
 
 
 if __name__ == '__main__':
