199da8eaf0d8b010e76a753648e0aa9ad46a2fa0
ksomemo <kntsmy@hotmail.co.jp>
2019-04-21 20:59:14 +0900

    Edit AtCoder ABC 078/080 C(AC:editorial)

--- atcoder/abc/080/C.py
+++ atcoder/abc/080/C.py
@@ -9,5 +9,32 @@ def main():
         for _ in range(N)
     ]
 
+    ans = f(N, F, P)
+    print(ans)
+
+
+def f(N, F, P):
+    """
+    N=100=10^2
+    曜日*AMPM=10
+        Fの組合せ=2^10≒1000=10^3
+    10^3*10^2=10^5
+    """
+    ans = -float("inf")
+    for x in range(1, 2 ** 10):
+        tmp = 0
+        for i in range(N):
+            c = 0
+            for j in range(10):
+                # bit onと店営業中の確認
+                if x & (2 ** j) and F[i][j]:
+                    c += 1
+            tmp += P[i][c]
+
+        ans = max(ans, tmp)
+
+    return ans
+
+
 if __name__ == '__main__':
     main()
